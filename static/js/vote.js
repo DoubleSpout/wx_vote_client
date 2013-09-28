97d78956e72d08fbff8bba15a82a362340853788
snoopyxdy <snoopyxdy@gmail.com>
2013-09-28 22:25:19 +0800

    client demo

--- static/js/vote.js
+++ static/js/vote.js
@@ -1,10 +1,24 @@
+var Get_QueryString_Plus = function(url){
+   var no_q = 1,
+      now_url = url && (url.split('?')[1] || no_q) || document.location.search.slice(1) || no_q;
+  if(now_url === no_q) return false;
+        var q_array = now_url.split('&'),
+       q_o = {},
+       v_array; 
+        for(var i=0;i<q_array.length;i++){
+              v_array = q_array[i].split('=');
+       q_o[v_array[0]] = v_array[1];
+        };
+        return q_o;
+    }
+
 $(function(){
-	var TEMPLATE = '<li id="{voteid}"><a href="{$voteimg}" rel="lightbox"><img src="{voteimg}" width="80" height="100"></a>'+
+	var TEMPLATE = '<li id="{voteid}"><a href="{voteimg}" rel="lightbox"><img src="{voteimg}" width="80" height="100"></a>'+
                    '<p><span>{votetitle}</span></br><span name="votenum"><b name="num">{votenum}</b>票</span>'+
             	   '<i name="vote" class="icon-thumbs-up icon-3x pull-right" data-toggle="modal" data="{voteid}" href="#myModal"></i>'+
           		   '</p></li>'
 	
-	var appuserid = '123456';
+	var appuserid = Get_QueryString_Plus().appuserid || '123456';
 	var genUrl = function(url){
 		return url+'?appuserid='+appuserid
 	}
@@ -24,12 +38,14 @@ $(function(){
 		},'json')	
 	}
 	
+	var isVoted = false;
 	var get_vote_status = function(cb){
 		$.get(genUrl('/votestatus'),{r:Math.random()}, function(d){
-			if(d.err) return alert(d.err)
+			if(d.err) return alert(d.data)
 			var array = d.data;
 			for(var i=0; i<array.length;i++){
 				$('#'+array[i]["voteId"]).find('.icon-thumbs-up').addClass('sel');	
+				isVoted = true;
 			}
 			cb && cb()	
 		},'json')	
@@ -38,6 +54,7 @@ $(function(){
 	
 	var voteid;
 	$('#tiles').delegate('i[name="vote"]', 'click', function(){
+		if(isVoted) return false
 		voteid = $(this).attr('data');
 	})
 
@@ -48,7 +65,8 @@ $(function(){
 		$.post(genUrl('/vote'), {"voteid":voteid}, function(d){
 			if(d.err){
 				hasvote = false;
-				return alert('投票失败')
+				$('#submit_close').trigger('click');
+				return alert(d.data)
 			}
 				var $b = $('#'+voteid).find('b[name="num"]')
 				var n = ~~($b.text());
@@ -61,5 +79,31 @@ $(function(){
 		return false;
 	})
 	
-	init(get_vote_status);
+	var binding = function(){
+		$('#tiles').imagesLoaded(function() {
+	        // Prepare layout options.
+	        var options = {
+	          autoResize: true, // This will auto-update the layout when the browser window is resized.
+	          container: $('#main'), // Optional, used for some extra CSS styling
+	          offset: 15, // Optional, the distance between grid items
+	          outerOffset: 10, // Optional, the distance to the containers border
+	          itemWidth: 90 // Optional, the width of a grid item
+	        };
+
+	        // Get a reference to your grid items.
+	        var handler = $('#tiles li');
+
+	        // Call the layout function.
+	        handler.wookmark(options);
+
+	        // Init lightbox
+	        $('a', handler).colorbox({
+	          rel: 'lightbox'
+	        });
+    	})
+	}
+
+	init(function(){
+		get_vote_status(binding)
+	});
 })
